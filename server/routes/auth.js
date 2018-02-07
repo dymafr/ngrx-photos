6f3ecbc4ffa95f5e26758a57e15eaa4eff04e3d6
hades <quentindevial19@gmail.com>
2018-02-07 21:57:49 +0100

    fix

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,15 +7,6 @@ const fs = require('fs');
 const RSA_KEY_PRIVATE = fs.readFileSync('./rsa/key');
 const RSA_KEY_PUBLIC = fs.readFileSync('./rsa/key.pub');
 
-function createToken(user) {
-  return jwt.sign({}, RSA_KEY_PRIVATE, {
-    algorithm: 'RS256',
-    expiresIn: '15s',
-    subject: user._id.toString()
-  })
-}
-
-
 router.get('/refresh-token',(req , res) => {
   const token = req.headers.authorization;
   if (token) {
@@ -24,21 +15,27 @@ router.get('/refresh-token',(req , res) => {
         console.log('err : ', err);
         res.status(401).json('tooooooken expire');
       } else {
-        User.findOne({ '_id': decoded.sub }).exec( (err , user) => {
-          const newToken = createToken(user);
-          res.json(newToken);
+        const newToken = jwt.sign({}, RSA_KEY_PRIVATE, {
+          algorithm: 'RS256',
+          expiresIn: '15s',
+          subject: decoded.sub
         })
+        res.json(newToken);
       }
     })
   } else {
-    res.status(401).json('no token');
+    res.stats(401).json('no token');
   }
 })
 
 router.post('/signin', (req, res) => {
   User.findOne({ 'email': req.body.email }).exec( (err, user) => {
     if (user && bcrypt.compareSync(req.body.password, user.password)) {
-      const token = createToken(user);
+      const token = jwt.sign({}, RSA_KEY_PRIVATE, {
+        algorithm: 'RS256',
+        expiresIn: '15s',
+        subject: user._id.toString()
+      })
       res.status(200).json(token);
     } else {
       res.status(401).json('signin fail !');
